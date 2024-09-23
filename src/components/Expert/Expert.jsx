cb91ebfdf63780761956b40cd30ab01b7e1c7bf2
Tomek Sochacki <zbudujmydompl@gmail.com>
2024-09-23 20:29:23 +0200

    fixed img alt

--- src/components/Expert/Expert.jsx
+++ src/components/Expert/Expert.jsx
@@ -3,7 +3,7 @@ import styles from "./Expert.module.css";
 const Expert = () => {
   return (
     <div className={styles.container}>
-      <img src="/images/expert.jpg" className={styles.photo} />
+      <img src="/images/expert.jpg" className={styles.photo} alt="expert John" />
       <div>
         <h2>John</h2>
         <p>description</p>
